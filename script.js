4efcd1576467d7ef8e3df7c5124c14c74d1b1762
PranayTyagi1 <>
2021-04-29 14:01:26 +0000

    added missing space

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('button').addEventListener('click', function() {
+document.getElementById('button').addEventListener('click', function () {
   alert(document.getElementById('textbox').value)
 })
